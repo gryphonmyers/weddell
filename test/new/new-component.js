afb0d9dc7722f3a71924dd4a2797766cef1ce9bf
Gryphon Myers <gryphon@homunkul.us>
2020-04-12 22:22:54 -0700

    more test

--- test/new/new-component.js
+++ test/new/new-component.js
@@ -417,4 +417,56 @@ ava('Component renders content into slot', async test => {
     test.is(comp.html.replace(/\s\s+/g, ' ').trim(), `<div class="fi">1</div> <div class="win">2</div> <template id="08i6u3"> <div class="fickle">sho</div> <div class="wigga">2</div> </template>`)
 })
 
-ava.todo('State change used by slot content causes rerender')
+ava('State change used by slot content causes rerender', async test => {
+    class MyComponent extends Component {
+        store({reactive}) {
+            return {
+                foo: 1,
+                bar: reactive(2)
+            }
+        }
+
+        template({html, state, component}) {
+            return html`
+                <div class="fi">${state.foo}</div>
+                ${component('my-sub-component', {id: "08i6u3"}, html`
+                    <div class="wigga">${state.bar}</div>
+                `)}
+            `
+        }
+
+        static get components() {
+            return {
+                'my-sub-component': class extends Component {
+                    store({reactive}) {
+                        return {
+                            bus: reactive('sho')
+                        }
+                    }
+
+                    template({html, state, slot}){
+                        return html`
+                            <div class="fickle">${state.bus}</div>
+                            ${slot()}
+                        `
+                    }
+                }
+            }
+        }
+    }
+
+    const comp = new MyComponent();
+    const evts = [];
+    comp.on('htmlchange', evt => evts.push(evt))
+
+    await comp.init();
+
+    test.is(comp.html.replace(/\s\s+/g, ' ').trim(), `<div class="fi">1</div> <template id="08i6u3"> <div class="fickle">sho</div> <div class="wigga">2</div> </template>`)
+
+    comp.state.bar++
+
+    await comp.renderPromise
+
+    test.is(comp.html.replace(/\s\s+/g, ' ').trim(), `<div class="fi">1</div> <template id="08i6u3"> <div class="fickle">sho</div> <div class="wigga">3</div> </template>`)
+
+})
